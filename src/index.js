597a02a48ef8abc40c77ad8eed7f97e75cc8f1a2
David Olurebi <olurebidavid@gmail.com>
2024-05-05 11:29:28 +0300

    Hello World print

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h3
+      style={{
+        width: "100%",
+        textAlign: "center",
+        height: "80vh",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      Hello World
+    </h3>
+    {/*<App />*/}
   </React.StrictMode>
 );
