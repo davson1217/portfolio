2e2ac296fcd313ea6125211b205cd0f182f3b309
David Olurebi <olurebidavid@gmail.com>
2024-06-08 18:49:26 +0300

    CV Update

--- src/components/cv-download-button.js
+++ src/components/cv-download-button.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import '../styles/download-btn.css';
+import React from "react";
+import "../styles/download-btn.css";
 
 const CvDownloadButton = () => {
-    return (
-        <a className="bg-success cv-download-btn p-1" href="My_Resume.pdf" download="david_olurebi_cv.pdf">
-            Download CV
-        </a>
-    );
+  return (
+    <a
+      className="bg-success cv-download-btn p-1"
+      href="Resume_.pdf"
+      download="david_olurebi_cv.pdf"
+    >
+      Download CV
+    </a>
+  );
 };
 
 export default CvDownloadButton;
